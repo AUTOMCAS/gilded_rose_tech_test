286c7e2950dcbfca296b3b68d1d68ed2fb5c39f2
Chris <caston88@gmail.com>
2022-11-16 10:57:20 +0000

    Add: Passing test: updateQuality() returns an item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -10,4 +10,16 @@ describe("Gilded Rose", () => {
       expect(item.quality).toBe(20);
     });
   });
+
+  describe("Shop", () => {
+    describe("updateQuality()", () => {
+      test("returns an item", () => {
+        const items = [new Item("+5 Dexterity Vest", 10, 20)]
+        const gildedRose = new Shop(items);
+        const returnedItems = gildedRose.updateQuality();
+
+        expect(returnedItems[0].name).toBe("+5 Dexterity Vest")
+      });
+    });
+  });
 });

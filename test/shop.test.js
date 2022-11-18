2c50d3c674dcdb0da54d025f91cdf9432bcc9707
Chris <caston88@gmail.com>
2022-11-18 13:57:14 +0000

    Refactor: Reintroduce passing tests for backstage pass item to Shop class

--- test/shop.test.js
+++ test/shop.test.js
@@ -44,14 +44,6 @@ describe("Shop", () => {
       expect(backstagePass.sellIn).toBe(1);
     });
 
-    test("reduces Aged Brie sellIn by 1 when quality is more than or equal to 50", () => {
-      const items = [new Item("Aged Brie", 2, 50)];
-      const gildedRose = new Shop(items);
-
-      const returnedItems = gildedRose.updateQuality();
-      expect(returnedItems[0].sellIn).toBe(1);
-    });
-
     test("sellIn is not reduced for Sulfuras", () => {
       const items = [
         new Item("Sulfuras, Hand of Ragnaros", 0, 80),
@@ -73,96 +65,96 @@ describe("Shop", () => {
 
       expect(returnedItems[0].quality).toBe(50);
     });
+
+    test("updates Aged Brie", () => {
+      const items = [new Item("Aged Brie", 2, 0)];
+      const gildedRose = new Shop(items);
+
+      const agedBrie = gildedRose.updateQuality()[0];
+      expect(agedBrie.sellIn).toBe(1);
+      expect(agedBrie.quality).toBe(1);
+    });
+
+    test("updates Aged Brie when quality is more than or equal to 50", () => {
+      const items = [new Item("Aged Brie", 2, 50)];
+      const gildedRose = new Shop(items);
+
+      const agedBrie = gildedRose.updateQuality()[0];
+      expect(agedBrie.sellIn).toBe(1);
+    });
+
+    test("Updates backstage pass when sell by is more than 10", () => {
+      const items = [
+        new Item("Backstage passes to a TAFKAL80ETC concert", 12, 6),
+      ];
+      const gildedRose = new Shop(items);
+
+      const backstagePass = gildedRose.updateQuality()[0];
+
+      expect(backstagePass.quality).toBe(7);
+    });
+
+    test("Updates backstage pass when sell by is less than or equal to 10", () => {
+      const items = [
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 6),
+      ];
+      const gildedRose = new Shop(items);
+
+      const backstagePass = gildedRose.updateQuality()[0];
+
+      expect(backstagePass.quality).toBe(8);
+    });
+
+    test("Updates backstage pass when sell by is less than or equal to  5", () => {
+      const items = [
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 6),
+      ];
+      const gildedRose = new Shop(items);
+
+      const backstagePass = gildedRose.updateQuality()[0];
+
+      expect(backstagePass.quality).toBe(9);
+    });
+    test("Updates backstage pass when when sell by has passed ", () => {
+      const items = [
+        new Item("Backstage passes to a TAFKAL80ETC concert", -1, 6),
+      ];
+      const gildedRose = new Shop(items);
+
+      const backstagePass = gildedRose.updateQuality()[0];
+
+      expect(backstagePass.quality).toBe(0);
+    });
   });
 
-//   describe("updateSulfuras()", () => {
-//     xtest("increases quality by 1 if quality is under 50", () => {
-//       const item = new Item("Sulfuras, Hand of Ragnaros", 0, 49);
-//       const gildedRose = new Shop(item);
 
-//       gildedRose.updateSulfuras(item);
 
-//       expect(item.quality).toBe(50);
-//     });
-//   });
+  //   describe("updateStandardItem()", () => {
+  //     test("reduces standard item quality by 1", () => {
+  //       const item = new Item("+5 Dexterity Vest", 10, 20);
+  //       const gildedRose = new Shop(item);
 
-//   describe("updateAgedBrie()", () => {
-//     test("increases Aged Brie quality by 1", () => {
-//       const item = new Item("Aged Brie", 2, 0);
-//       const gildedRose = new Shop(item);
+  //       gildedRose.updateStandardItem(item);
 
-//       gildedRose.updateAgedBrie(item);
+  //       expect(item.quality).toBe(19);
+  //     });
 
-//       expect(item.quality).toBe(1);
-//     });
-//   });
+  //     test("Quality cannot become negative", () => {
+  //       const item = new Item("+5 Dexterity Vest", 10, 0);
+  //       const gildedRose = new Shop(item);
 
-//   describe("updateBackstagePass()", () => {
-//     test("quality increases by 1 when sellIn is more than 10", () => {
-//       const item = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 6);
-//       const gildedRose = new Shop(item);
+  //       gildedRose.updateStandardItem(item);
 
-//       gildedRose.updateBackstagePass(item);
+  //       expect(item.quality).toBe(0);
+  //     });
 
-//       expect(item.quality).toBe(7);
-//     });
+  //     test("Quality degrades twice as fast when sellIn is less than 0", () => {
+  //       const item = new Item("+5 Dexterity Vest", -1, 10);
+  //       const gildedRose = new Shop(item);
 
-//     test("quality increases by 2 when sellIn is less than or equal to 10", () => {
-//       const item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 6);
-//       const gildedRose = new Shop(item);
+  //       gildedRose.updateStandardItem(item);
 
-//       gildedRose.updateBackstagePass(item);
-
-//       expect(item.quality).toBe(8);
-//     });
-
-//     test("quality increases by 3 when sellIn is less than or equal to 5", () => {
-//       const item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 6);
-//       const gildedRose = new Shop(item);
-
-//       gildedRose.updateBackstagePass(item);
-
-//       expect(item.quality).toBe(9);
-//     });
-
-//     test("quality reduces to 0 when sell in has passed ", () => {
-//       const items = [
-//         new Item("Backstage passes to a TAFKAL80ETC concert", -1, 6),
-//       ];
-//       const gildedRose = new Shop(items);
-
-//       const returnedItems = gildedRose.updateQuality();
-
-//       expect(returnedItems[0].quality).toBe(0);
-//     });
-//   });
-
-//   describe("updateStandardItem()", () => {
-//     test("reduces standard item quality by 1", () => {
-//       const item = new Item("+5 Dexterity Vest", 10, 20);
-//       const gildedRose = new Shop(item);
-
-//       gildedRose.updateStandardItem(item);
-
-//       expect(item.quality).toBe(19);
-//     });
-
-//     test("Quality cannot become negative", () => {
-//       const item = new Item("+5 Dexterity Vest", 10, 0);
-//       const gildedRose = new Shop(item);
-
-//       gildedRose.updateStandardItem(item);
-
-//       expect(item.quality).toBe(0);
-//     });
-
-//     test("Quality degrades twice as fast when sellIn is less than 0", () => {
-//       const item = new Item("+5 Dexterity Vest", -1, 10);
-//       const gildedRose = new Shop(item);
-
-//       gildedRose.updateStandardItem(item);
-
-//       expect(item.quality).toBe(8);
-//     });
-//   });
+  //       expect(item.quality).toBe(8);
+  //     });
+  //   });
 });
